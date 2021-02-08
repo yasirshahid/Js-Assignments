76ca161ad332c7429070950c0640222d8c103cce
yasir shahid <yasirshahid5@gmail.com>
2021-02-08 17:27:16 +0500

    add assignment 5

--- 05Assignment/Ass05.js
+++ 05Assignment/Ass05.js
@@ -1,28 +1,12 @@
-var course1 = prompt("Enter course 1 Marks");
+var course1 = parseInt(prompt("Enter your Course 1 Marks"));
+var course2 = parseInt(prompt("Enter your Course 2 Marks"));
+var course3 = parseInt(prompt("Enter your Course 3 Marks"));
+var course4 = parseInt(prompt("Enter your Course 4 Marks"));
+var course5 = parseInt(prompt("Enter your Course 5 Marks"));
+var totalMarks = 500
 
-if (course1 <= 100) {
-    cou1 = course1 
-}
-else if (cou1 != course1){
-    alert("Not a valid marks")
-}
+var obtainedMarks = (course1 + course2 + course3 + course4 + course5);
 
-console.log(cou1);
-console.log(course1)
+var perCal = (obtainedMarks * 100 / totalMarks);
 
-
-function course1 ( sub1 ) {
-    if (sub1 <= 100){
-        
-    }
-}
-
-var course1 = parseInt(prompt("enter the course1 Marks"));
-var course2 = parseInt(prompt("enter the course1 Marks"));
-var course3 = parseInt(prompt("enter the course1 Marks"));
-var course4 = parseInt(prompt("enter the course1 Marks"));
-var course5 = parseInt(prompt("enter the course1 Marks"));
-
-var add = course1+course2+course3+course4+course5
-
-
+alert("Your percentage is" + " " + perCal);
